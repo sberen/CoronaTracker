4dadb9d503fcc2084a7f1e02aa9bf1d35575d024
Samuel Berensohn <samberensohn@gmail.com>
2020-05-18 01:13:04 -0700

    Final Commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import {fetchDailyData} from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
+import CountryPicker from '../CountryPicker/CountryPicker';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
     const [ dailyData, setDailyData ] = useState([]);
 
     useEffect(() => {
@@ -12,7 +13,9 @@ const Chart = () => {
         }
 
         fetchAPI();
-    });
+    }, []);
+
+    console.log(confirmed)
 
     const lineChart = (
         dailyData.length
@@ -35,9 +38,35 @@ const Chart = () => {
                 }}
             />) : null
     );
+
+    const barChart  = (
+        confirmed
+        ? (
+            <Bar 
+                data={{
+                    labels:['Infected', 'Recovered', 'Deaths'],
+                    datasets: [{
+                        label: 'People',
+                        backgroundColor: [
+                            'rgba(0, 0, 255, 0.5)',
+                            'rgba(0, 255, 0, 0.5)',
+                            'rgba(255, 0, 0, 0.5)'
+                        ],  
+                        data:[confirmed.value, recovered.value, deaths.value]
+                    }]
+                }}
+                options={{
+                    legend: { display: false },
+                    title: { display: true, text:`Current State in ${country}`}
+                }}
+            />
+
+        ) : null
+    );
+
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country !== 'global' && country ? barChart : lineChart}
         </div>
     );
 }
